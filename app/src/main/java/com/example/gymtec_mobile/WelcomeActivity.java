99dbcbbed706312a3db3f4cc8adbde75d0e0cd49
geogardow <85602062+geogardow@users.noreply.github.com>
2023-05-04 17:58:10 -0600

    LogInPage

---
+++ app/src/main/java/com/example/gymtec_mobile/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.gymtec_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_activity);
+    }
+}
