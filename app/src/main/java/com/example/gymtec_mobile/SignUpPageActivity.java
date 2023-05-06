13081005c228ff2d589febaea02b199ebae6f15d
geogardow <85602062+geogardow@users.noreply.github.com>
2023-05-06 00:30:06 -0600

    Sign Up done

---
+++ app/src/main/java/com/example/gymtec_mobile/SignUpPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.gymtec_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_page_activity);
+    }
+}
